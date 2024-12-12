041faaada3a5d5783662da962c8a4e2114faf3f9
Stephenworkplace <139206587+hanyuany14@users.noreply.github.com>
2024-12-12 23:37:25 +0800

    finish medium

---
+++ src/backend/test.py
@@ -0,0 +1,44 @@
+{
+    "title": "OKX MemeFi Airdrop Checker: Check Your Rewards Today",
+    "title_detail": {
+        "type": "text/plain",
+        "language": None,
+        "base": "https://medium.com/feed/tag/technology",
+        "value": "OKX MemeFi Airdrop Checker: Check Your Rewards Today",
+    },
+    "summary": '<div class="medium-feed-item"><p class="medium-feed-image"><a href="https://cryptoairdropped193.medium.com/okx-memefi-airdrop-checker-check-your-rewards-today-5eece18c0350?source=rss------technology-5"><img src="https://cdn-images-1.medium.com/max/1344/0*d323rB7zOsos0Cc_.jpg" width="1344" /></a></p><p class="medium-feed-link"><a href="https://cryptoairdropped193.medium.com/okx-memefi-airdrop-checker-check-your-rewards-today-5eece18c0350?source=rss------technology-5">Continue reading on Medium »</a></p></div>',
+    "summary_detail": {
+        "type": "text/html",
+        "language": None,
+        "base": "https://medium.com/feed/tag/technology",
+        "value": '<div class="medium-feed-item"><p class="medium-feed-image"><a href="https://cryptoairdropped193.medium.com/okx-memefi-airdrop-checker-check-your-rewards-today-5eece18c0350?source=rss------technology-5"><img src="https://cdn-images-1.medium.com/max/1344/0*d323rB7zOsos0Cc_.jpg" width="1344" /></a></p><p class="medium-feed-link"><a href="https://cryptoairdropped193.medium.com/okx-memefi-airdrop-checker-check-your-rewards-today-5eece18c0350?source=rss------technology-5">Continue reading on Medium »</a></p></div>',
+    },
+    "links": [
+        {
+            "rel": "alternate",
+            "type": "text/html",
+            "href": "https://cryptoairdropped193.medium.com/okx-memefi-airdrop-checker-check-your-rewards-today-5eece18c0350?source=rss------technology-5",
+        }
+    ],
+    "link": "https://cryptoairdropped193.medium.com/okx-memefi-airdrop-checker-check-your-rewards-today-5eece18c0350?source=rss------technology-5",
+    "id": "https://medium.com/p/5eece18c0350",
+    "guidislink": False,
+    "tags": [
+        {"term": "bitcoin", "scheme": None, "label": None},
+        {"term": "crypto", "scheme": None, "label": None},
+        {"term": "technology", "scheme": None, "label": None},
+        {"term": "blockchain", "scheme": None, "label": None},
+        {"term": "airdrop", "scheme": None, "label": None},
+    ],
+    "authors": [{"name": "crypto airdrop ped193"}],
+    "author": "crypto airdrop ped193",
+    "author_detail": {"name": "crypto airdrop ped193"},
+    "published": "Thu, 12 Dec 2024 14:45:06 GMT",
+    "published_parsed": time.struct_time(
+        tm_year=2024, tm_mon=12, tm_mday=12, tm_hour=14, tm_min=45, tm_sec=6, tm_wday=3, tm_yday=347, tm_isdst=0
+    ),
+    "updated": "2024-12-12T14:45:06.203Z",
+    "updated_parsed": time.struct_time(
+        tm_year=2024, tm_mon=12, tm_mday=12, tm_hour=14, tm_min=45, tm_sec=6, tm_wday=3, tm_yday=347, tm_isdst=0
+    ),
+}
