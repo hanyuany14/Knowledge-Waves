1a2eefea62bbb0a870ca7baa79b28917e7d134f1
Stephenworkplace <139206587+hanyuany14@users.noreply.github.com>
2024-12-29 23:29:36 +0800

    add todo comment

--- src/backend/summary.py
+++ src/backend/summary.py
@@ -82,6 +82,59 @@ class Summarization:
         return selected_tags, summary, articles
 
     def llm_summary(self, target_articles: dict[str, list[tuple[str, str]]]) -> str:
+        """
+        1. 現在總結是對每一個 repo 或是文章做總結，但太長了，現在要改成對一個 source 就做一個總節
+        ex.
+        github:
+        今天關於 rag 資訊有 memory, prompt 層面。例如 abc/rag-memory 提出一個全新的 memory 演算法，加入了短期演算法改善了模型的記憶。
+
+        csdn:
+        xxx
+
+        medium:
+        xxx
+
+        -> 建議解決方案：現在是一個 for 迴圈，可以再一個 for 迴圈再一次總結，兩個是不同的 prompt
+
+
+        2. 內容中要如果提到文章內容，要變成超連結型態。
+        ex.
+        今天關於 rag 資訊有 memory, prompt 層面。例如<a href=""https://github.com"">abc/rag-memory</a>提出一個全新的 memory 演算法，
+
+        -> 所以現在的參數要額外傳入每個文章的 url，並且一起送進去給第一層的 api
+        -> 要在第二層 prompt 加入要用超連結的格式指引，可以給他上面的範例。
+
+        兩層迴圈輸入與輸出：
+        第一層輸入：
+        url: https:////
+        content: xxxxx
+
+        url: https:////
+        content: xxxxx
+
+        第一層輸出 & 第二層輸入：
+        url: https:////
+        summay: xxxxx
+
+        url: https:////
+        summay: xxxxx
+
+        第二層輸出：
+        source_summay: 今天關於 rag 資訊有 memory, prompt 層面。例如<a href=""https://github.com"">abc/rag-memory</a>提出一個全新的 memory 演算法，
+
+        最後就是 return:
+
+        return {
+            'github':source_summay,
+            'csdn: source_summay,
+            "medium": source_summay,
+        }
+
+
+        第一層的 llm 的 prompt 大概可以是「你是一個友善的學者，負責將文章總結成有意義且重點的段落。請使用繁體中文回覆。」
+        第二層的 llm 的 prompt 可以是「你需要將每一個文章總結整合成一段敘述，然後標題（repo 標題或是文章標題）要用 <a href> 中間加入 url」
+
+        """
         prompt = ChatPromptTemplate.from_messages(
             [
                 ("system", "你是一個友善的學者，負責將文章總結成有意義且重點的段落。請使用繁體中文回覆。"),
